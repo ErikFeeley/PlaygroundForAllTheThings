c5ed08c7b666f8d2a9b8e47a728fbe4459157b1f
Erik Feeley <efeeley@vectorform.com>
2016-12-19 14:00:48 -0500

    added new project but might dump it... playing a lot in the startup.cs

---
+++ CoreCQRSApp/src/CoreCQRSApp/Data/ToDoContext.cs
@@ -0,0 +1,22 @@
+﻿using CoreCQRSApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreCQRSApp.Data
+{
+    public class ToDoContext : IdentityDbContext<ToDoUser>
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) 
+            : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // can add customization for entities here
+        }
+    }
+}
--- CoreCQRSApp/src/CoreCQRSApp/Startup.cs
+++ CoreCQRSApp/src/CoreCQRSApp/Startup.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CoreCQRSApp.Data;
+using CoreCQRSApp.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -7,11 +12,30 @@ namespace CoreCQRSApp
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment environment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<ToDoContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ToDoUser, IdentityRole>()
+                .AddEntityFrameworkStores<ToDoContext>()
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,13 +50,7 @@ namespace CoreCQRSApp
 
             app.UseStaticFiles();
 
-            app.UseMvc(routeConfig =>
-            {
-                routeConfig.MapRoute(
-                    "default",
-                    "{controller=Home}/{action=Index}/{id?}"
-                );
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
