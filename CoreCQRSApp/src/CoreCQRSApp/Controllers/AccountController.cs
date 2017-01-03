730ee7efdc01331cd70390cbe462a3b36f81cf80
Erik Feeley <efeeley@vectorform.com>
2017-01-03 13:18:32 -0500

    working with registration, trying to improve the default boilerplate code and how returnUrls are handled

--- CoreCQRSApp/src/CoreCQRSApp/Controllers/AccountController.cs
+++ CoreCQRSApp/src/CoreCQRSApp/Controllers/AccountController.cs
@@ -1,15 +1,57 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using CoreCQRSApp.Models;
+using CoreCQRSApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreCQRSApp.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
+        private readonly UserManager<ToDoUser> _userManager;
+        private readonly SignInManager<ToDoUser> _signInManager;
+
+        public AccountController(UserManager<ToDoUser> userManager, SignInManager<ToDoUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         [AllowAnonymous]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+
+            var user = new ToDoUser { UserName = registerViewModel.Email, Email = registerViewModel.Email };
+            var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            AddErrors(result);
+
+            return View(registerViewModel);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
