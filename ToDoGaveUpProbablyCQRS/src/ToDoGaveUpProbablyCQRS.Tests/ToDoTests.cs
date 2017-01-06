23e391945c2602ea1a67a42bc163112f20d8408e
Erik Feeley <efeeley@vectorform.com>
2017-01-06 16:54:45 -0500

    logging and trying to get unit testing with moq going

---
+++ ToDoGaveUpProbablyCQRS/src/ToDoGaveUpProbablyCQRS.Tests/ToDoTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace ToDoGaveUpProbablyCQRS.Tests
+{
+    [TestFixture]
+    public class ToDoTests
+    {
+        [Test]
+        public void TestTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void MoQTestTest()
+        {
+        }
+    }
+}
