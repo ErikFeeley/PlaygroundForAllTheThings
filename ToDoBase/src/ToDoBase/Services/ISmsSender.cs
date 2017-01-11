aaeca1d2cfb5cd3becb39f9c1fc027cfea5dd14d
Erik Feeley <efeeley@vectorform.com>
2017-01-11 17:04:37 -0500

    added a todo base project which will mirror anthonys work more closely

---
+++ ToDoBase/src/ToDoBase/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoBase.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
