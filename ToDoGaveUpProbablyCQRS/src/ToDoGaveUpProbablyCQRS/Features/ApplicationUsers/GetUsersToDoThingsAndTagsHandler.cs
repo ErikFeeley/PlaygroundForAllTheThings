e4b18d99c2e81ad04b7ab4970f44033e29675731
Erik Feeley <efeeley@vectorform.com>
2017-01-17 13:36:58 -0500

    playing with ef linq query stuff again

--- ToDoGaveUpProbablyCQRS/src/ToDoGaveUpProbablyCQRS/Features/ApplicationUsers/GetUsersToDoThingsAndTagsHandler.cs
+++ ToDoGaveUpProbablyCQRS/src/ToDoGaveUpProbablyCQRS/Features/ApplicationUsers/GetUsersToDoThingsAndTagsHandler.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.Core;
 using ToDoGaveUpProbablyCQRS.Data;
 using ToDoGaveUpProbablyCQRS.Dtos;
 using ToDoGaveUpProbablyCQRS.Models;
@@ -21,13 +22,13 @@ namespace ToDoGaveUpProbablyCQRS.Features.ApplicationUsers
         public async Task<UserDto> Handle(GetUsersToDoThingsAndTagsQuery message)
         {
             // still not there yet.
-            var result = await _dbContext.ToDoThingTags
-                .Include(tdtt => tdtt.Tag)
-                .Include(tdtt => tdtt.ToDoThing)
-                .ThenInclude(tdt => tdt.ApplicationUser)
-                .Where(tdt => tdt.ToDoThing.ApplicationUser.Id == message.Id)
-                .Select(tdtt => new { tdtt.ToDoThing.ApplicationUser, tdtt.ToDoThing.ApplicationUser.ToDoThings })
-                .FirstOrDefaultAsync();
+            //var result = await _dbContext.ToDoThingTags
+            //    .Include(tdtt => tdtt.Tag)
+            //    .Include(tdtt => tdtt.ToDoThing)
+            //    .ThenInclude(tdt => tdt.ApplicationUser)
+            //    .Where(tdt => tdt.ToDoThing.ApplicationUser.Id == message.Id)
+            //    .Select(tdtt => new { tdtt.ToDoThing.ApplicationUser, tdtt.ToDoThing.ApplicationUser.ToDoThings })
+            //    .FirstOrDefaultAsync();
 
             //var result = await _dbContext.Users
             //    .Include(user => user.ToDoThings)
@@ -36,9 +37,38 @@ namespace ToDoGaveUpProbablyCQRS.Features.ApplicationUsers
             //    .Where(user => user.Id == message.Id)
             //    .FirstOrDefaultAsync();
 
+
+            //this query actually generates one sql statement with just joins and no crazy shit but it only gets 1 todothing and 1 tag instead of all of em.
+            //var result = await _dbContext.ToDoThingTags
+            //    .Include(tdtt => tdtt.Tag)
+            //    .Include(tdtt => tdtt.ToDoThing)
+            //    .ThenInclude(tdt => tdt.ApplicationUser)
+            //    .FirstOrDefaultAsync(tdtt => tdtt.ToDoThing.ApplicationUser.Id == message.Id);
+
+            // this also unsuprisingly enough builds a stupid ass query.
+            //var result = await _dbContext.ToDoThingTags
+            //    .Include(tdtt => tdtt.ToDoThing.ApplicationUser)
+            //    .ThenInclude(user => user.ToDoThings)
+            //    .ThenInclude(tdt => tdt.ToDoThingTags)
+            //    .ThenInclude(tdtt => tdtt.Tag)
+            //    .FirstOrDefaultAsync(tdtt => tdtt.ToDoThing.ApplicationUser.Id == message.Id);
+
+            // plz gawd
+            var result = await _dbContext.ToDoThingTags
+                .Include(tdtt => tdtt.ToDoThing.ApplicationUser)
+                .FirstOrDefaultAsync(tdtt => tdtt.ToDoThing.ApplicationUser.Id == message.Id);
+
+
+            // this biulds a stupid ass query.
+            //var result = await _dbContext.Users
+            //    .Include(user => user.ToDoThings)
+            //    .ThenInclude(tdt => tdt.ToDoThingTags)
+            //    .ThenInclude(tdtt => tdtt.Tag)
+            //    .FirstOrDefaultAsync(user => user.Id == message.Id);
+
             var userDto = new UserDto();
 
-            userDto.Email = result.ApplicationUser.Email;
+            userDto.Email = result.ToDoThing.ApplicationUser.Id;
 
             return userDto;
         }
