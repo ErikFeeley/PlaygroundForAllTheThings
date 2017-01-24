62be689a9825538d26da59496733d724cf9c0cea
Erik Feeley <efeeley@vectorform.com>
2017-01-24 15:04:26 -0500

    moar refactoring this will work mostly... i think... and i should be able to replicate it

---
+++ EFTestTakeTwo/src/EFTestTakeTwo/Properties/PublishProfiles/AwesomeProfile-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = '54ea8681-355e-4c8c-9e94-d4d99a5be9fc'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
