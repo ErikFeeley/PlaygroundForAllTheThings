5d7396318fae8f61906aca447922d8b0b7ac54e2
ErikFeeley <ehfeeley@gmail.com>
2017-02-12 19:35:52 -0500

    playing around with the new VS2017

---
+++ PlayingWithVS2017RC/PlayingWithVS2017RC/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayingWithVS2017RC.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
