570798e9ac14e1d14561e556753f1aafc5c884db
ErikFeeley <ehfeeley@gmail.com>
2017-02-12 20:44:40 -0500

    derp and still wtf are these .d.ts files doing. I aint writing no typescript right now

---
+++ PlayingWithVS2017RC/PlayingWithVS2017RC/Handlers/GetMyStuffHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using PlayingWithVS2017RC.Messages.Queries;
+using System.Collections.Generic;
+using PlayingWithVS2017RC.Data;
+
+namespace PlayingWithVS2017RC.Handlers
+{
+    public class GetMyStuffHandler : IAsyncRequestHandler<GetMyStuffQuery, IEnumerable<string>>
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public GetMyStuffHandler(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<string>> Handle(GetMyStuffQuery message)
+        {
+            return new List<string>();
+        }
+    }
+}
---
+++ PlayingWithVS2017RC/PlayingWithVS2017RC/Messages/Queries/GetMyStuffQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace PlayingWithVS2017RC.Messages.Queries
+{
+    public class GetMyStuffQuery : IRequest<IEnumerable<string>>
+    {
+        
+    }
+}
