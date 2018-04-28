8a7ee347447a069094bac1ba8c730a617e651a68
ErikFeeley <ehfeeley@gmail.com>
2018-04-28 02:34:24 -0400

    koa seems pretty cool

--- koa-test/src/index.ts
+++ koa-test/src/index.ts
@@ -1,8 +1,22 @@
-import Koa = require('koa');
+import * as Koa from 'koa';
 
 const app = new Koa();
 
-app.use((ctx) => {
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+app.use(async (ctx) => {
   ctx.body = 'Hello koa';
 });
 
